756826c40f8492bd840b584427a8813f17a6a2ca
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-04-13 15:06:48 +0300

    777

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { addToEnvironment, type IRuntimeEnvironment } from './environment';
 import './style.css';
 import App from './app-test.vue';
 
-axios.get(`/environment.json?${new Date().getTime()}`).then(async (data: AxiosResponse<IRuntimeEnvironment>) => {
+axios.get('/environment.json').then(async (data: AxiosResponse<IRuntimeEnvironment>) => {
 	addToEnvironment(data.data);
 });
 createApp(App).mount('#app');
