831eef4777a950de25b7429bf26914ad690817d1
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-04-13 22:37:29 +0300

    ffff

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue';
 import axios, { AxiosResponse } from 'axios';
 
-import { addToEnvironment, type IRuntimeEnvironment } from './environment';
-
 import './style.css';
+import { addToEnvironment, type IRuntimeEnvironment } from './environment';
+// import { BASE_URL } from './constants';
+
 import App from './app-test.vue';
 
-axios.get('/environment.json').then(async (data: AxiosResponse<IRuntimeEnvironment>) => {
-	addToEnvironment(data.data);
-});
+// axios.get(`${BASE_URL}/environment.json`).then(async (data: AxiosResponse<IRuntimeEnvironment>) => {
+// 	addToEnvironment(data.data);
+// });
+console.log(import.meta.env.BASE_URL);
+console.log(import.meta.env.PROD);
+
 createApp(App).mount('#app');
