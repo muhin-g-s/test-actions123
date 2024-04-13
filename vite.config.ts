7d182b20368aa6124561475e96132797d69520d5
Мухин Григорий <muhin.g.s.00@gmail.com>
2024-04-13 15:53:23 +0300

    555

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	base: '/test-actions123/qwerty/',
+	// base: '/test-actions123/',
 });
